133451398ff9779f2f7fab8affe8a41f11ddd521
Reza Ghorbani <rg@rezaghorbani.dev>
2024-07-08 19:40:43 +0330

    Test fetch config and methods

--- src/xhr/fetch.test.ts
+++ src/xhr/fetch.test.ts
@@ -1,17 +1,157 @@
-import { ACCEPT_LANGUAGE } from "../lib/constants";
+import { ACCEPT_LANGUAGE, AUTHORIZATION } from "../lib/constants";
 import { KeyNotFoundError, NoKeyProvidedError } from "../lib/errors";
-import { mockGetUsers, resetMocks, users } from "../tests/mock";
+import {
+  mockCreateUser,
+  mockGetUsers,
+  resetMocks,
+  user,
+  users,
+} from "../tests/mock";
 import Fetch from "./fetch";
 
-describe("Initializing the fetch", () => {
-  //#region Locale
-
+describe("Headers CRUD", () => {
+  let fetch: Fetch;
   const globalLocale = "fr-FR";
+
+  beforeEach(() => {
+    fetch = new Fetch();
+  });
+  afterEach(() => {
+    resetMocks();
+  });
+
+  /**
+   * it should contain inits that has been set in the cunstructor
+   */
+  test("Cunstructor Initialization", async () => {
+    expect(fetch.header("Content-Type")).toEqual("application/json");
+  });
+
+  /**
+   * it should change the locale by header method
+   */
+  test("Set header", async () => {
+    fetch.header(ACCEPT_LANGUAGE, "fa-IR");
+
+    expect(fetch.header(ACCEPT_LANGUAGE)).toEqual("fa-IR");
+  });
+
+  /**
+   * it should throw error if key is null or empty
+   */
+  test("Check for empty header name before delete", async () => {
+    Fetch.global.locale = globalLocale;
+
+    const fetch = new Fetch();
+
+    expect(() => fetch.removeHeader("")).toThrow(NoKeyProvidedError);
+  });
+
+  /**
+   * it should throw error if key is not found
+   */
+  test("Validate header name before delete", async () => {
+    Fetch.global.locale = globalLocale;
+
+    const fetch = new Fetch();
+
+    expect(() => fetch.removeHeader("WrongKey")).toThrow(KeyNotFoundError);
+  });
+
+  /**
+   * it should remove the given attribute
+   */
+  test("Delete header", async () => {
+    Fetch.global.locale = globalLocale;
+
+    const fetch = new Fetch();
+    fetch.removeHeader(ACCEPT_LANGUAGE);
+
+    expect(fetch.header(ACCEPT_LANGUAGE)).toBeUndefined();
+  });
+});
+
+describe("Authorization", () => {
+  let fetch: Fetch;
+
+  beforeEach(() => {
+    fetch = new Fetch();
+  });
+  afterEach(() => {
+    resetMocks();
+  });
+
+  /**
+   * it should contain global auth token
+   */
+  test("Global auth token", async () => {
+    mockGetUsers();
+    const authToken = "Bearer testToken1234";
+    Fetch.global.authToken = authToken;
+
+    await fetch.get("http://example.com/data");
+
+    expect(fetch.header(AUTHORIZATION)).toEqual(authToken);
+  });
+
+  /**
+   * it should contain given auth token
+   */
+  test("Given auth token", async () => {
+    mockGetUsers();
+    const authToken = "Bearer testToken1234";
+
+    await fetch.setAuthToken(authToken).get("http://example.com/data");
+
+    expect(fetch.header(AUTHORIZATION)).toEqual(authToken);
+  });
+
+  /**
+   * it should be without credentials by default
+   */
+  test("Default credentials value", async () => {
+    mockGetUsers();
+
+    await fetch.get("http://example.com/data");
+
+    expect(Reflect.get(fetch, "init")["credentials"]).toBeUndefined();
+  });
+
+  /**
+   * it should contain global credentials
+   */
+  test("Global credentials", async () => {
+    mockGetUsers();
+    Fetch.global.includeCredentials = true;
+
+    await fetch.get("http://example.com/data");
+
+    expect(Reflect.get(fetch, "init")["credentials"]).toEqual("include");
+  });
+
+  /**
+   * it should contain given credentials
+   */
+  test("Given credentials", async () => {
+    mockGetUsers();
+
+    await fetch.includeCredentials().get("http://example.com/data");
+
+    expect(Reflect.get(fetch, "init")["credentials"]).toEqual("include");
+  });
+});
+
+describe("Locale", () => {
+  const globalLocale = "fr-FR";
+
+  afterEach(() => {
+    resetMocks();
+  });
 
   /**
    * it should set the locale globally
    */
-  it("should set the locale globally", async () => {
+  test("Global locale", async () => {
     Fetch.global.locale = globalLocale;
 
     const fetch = new Fetch();
@@ -21,7 +161,7 @@ describe("Initializing the fetch", () => {
   /**
    * it should override the default locale
    */
-  it("should override the default locale", async () => {
+  test("Overriding global locale", async () => {
     Fetch.global.locale = globalLocale;
 
     // Init with default locale
@@ -35,56 +175,6 @@ describe("Initializing the fetch", () => {
 
     expect(fetch.header(ACCEPT_LANGUAGE)).toEqual(globalLocale);
   });
-
-  //#endregion
-
-  //#region headers
-
-  /**
-   * it should change the locale by header method
-   */
-  it("should change the locale by header method", async () => {
-    const fetch = new Fetch();
-    fetch.header(ACCEPT_LANGUAGE, "fa-IR");
-
-    expect(fetch.header(ACCEPT_LANGUAGE)).toEqual("fa-IR");
-  });
-
-  /**
-   * it should throw error if key is null or empty
-   */
-  it("should throw error if key is null or empty", async () => {
-    Fetch.global.locale = globalLocale;
-
-    const fetch = new Fetch();
-
-    expect(() => fetch.removeHeader("")).toThrow(NoKeyProvidedError);
-  });
-
-  /**
-   * it should throw error if key is not found
-   */
-  it("should throw error if key is not found", async () => {
-    Fetch.global.locale = globalLocale;
-
-    const fetch = new Fetch();
-
-    expect(() => fetch.removeHeader("WrongKey")).toThrow(KeyNotFoundError);
-  });
-
-  /**
-   * it should remove the given attribute
-   */
-  it("should remove the given attribute", async () => {
-    Fetch.global.locale = globalLocale;
-
-    const fetch = new Fetch();
-    fetch.removeHeader(ACCEPT_LANGUAGE);
-
-    expect(fetch.header(ACCEPT_LANGUAGE)).toBeUndefined();
-  });
-
-  //#endregion
 });
 
 describe("fetchData", () => {
@@ -97,10 +187,7 @@ describe("fetchData", () => {
     resetMocks();
   });
 
-  //#region GET
-
-  // GET
-  it("fetches data from the given URL", async () => {
+  test("Get method", async () => {
     mockGetUsers();
 
     const data = await fetch.get("http://example.com/data");
@@ -113,5 +200,26 @@ describe("fetchData", () => {
     expect(url).toBe("http://example.com/data");
   });
 
-  //#endregion
+  test("POST method", async () => {
+    mockCreateUser();
+
+    const innerFetch = jest.spyOn(fetch as any, "fetch");
+
+    const { id } = await fetch.post<typeof user & { id: number }>(
+      "http://example.com/data",
+      user
+    );
+
+    expect(id).toEqual(3);
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+
+    expect(innerFetch).toHaveBeenCalledWith("http://example.com/data", {
+      method: "POST",
+      body: JSON.stringify(user),
+    });
+
+    const [url] = (global.fetch as any).mock.calls[0];
+    expect(url).toBe("http://example.com/data");
+  });
 });
